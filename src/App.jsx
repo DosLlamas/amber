196c3f5a261f1f4b036dc3b9dbf39d88a8357fb5
Nathan Foss <nathanfoss.dev@gmail.com>
2023-01-11 07:53:04 -0600

    adds styling to icon popup page and the amber logo with link to webstore

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Monke from './assets/popupLogo.png'
 import './App.css'
+import { chromeExtension } from '@crxjs/vite-plugin'
+// import { chromeExtension } from '@crxjs/vite-plugin'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // get the time and intention as the user submits the forms to chrome.storage
+  const [minutes, setMinutes] = useState(0)
+  chromeExtension.storage.sync.get(["minutes"], (result) => {
+    setMinutes(result.minutes)
+  }) 
+
+  const [intention, setIntention] = useState('Write an intention :)')
+  chromeExtension.storage.sync.get(["intention"], (result) => {
+    setIntention(result.intention)
+  })
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="amber-logo-container">
+      <a href="https://chrome.google.com/webstore/detail/amber-mindfulness/hdfdgocligofefcgklikgpjadbphlipm?hl=es-419" target="_blank">
+        <img src={Monke} alt="Amber Logo" className="amber-logo"></img>
+      </a>
+     </div>
     </div>
   )
 }
